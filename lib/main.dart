d0b330176ed07a6a2c41f92e75b8dbe16b77a675
Hafız <ramizovhafiz@gmail.com>
2024-02-26 21:48:26 +0300

    added new changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,24 @@ import 'package:riverpod_http/product/auth_controller/auth_controller.dart';
 import 'package:riverpod_http/product/init/init_app.dart';
 import 'feature/home/home_view.dart';
 import 'feature/login/login_view.dart';
+// Main changed
 
 void main() async {
   await AppInit.initApp();
-  runApp(ProviderScope(child:  MyApp()));
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
-   MyApp({super.key});
-
-
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authProvider);
 
-    final authState=ref.watch(authProvider);
-
-    Widget _getHome(){
-      if(authState.authStatus==AuthStatus.authenticated){
+    Widget _getHome() {
+      if (authState.authStatus == AuthStatus.authenticated) {
         return HomeView();
-      }else{
+      } else {
         return LoginView();
       }
     }
@@ -33,4 +31,3 @@ class MyApp extends ConsumerWidget {
     );
   }
 }
-
